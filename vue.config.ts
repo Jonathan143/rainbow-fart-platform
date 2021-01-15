5a24bcbdd71394dce66c6ce916a566ad811a3bcc
Jonathan143 <1439821144@qq.com>
2021-01-15 11:37:04 +0800

    chore: modify config

--- vue.config.ts
+++ vue.config.ts
@@ -1,24 +1,44 @@
-// 拼接路径
-// const resolve = dir => require('path').join(__dirname, dir)
+import path from 'path'
 
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
 // 增加环境变量
 process.env.VUE_APP_VERSION = require('./package.json').version
 
-const publicPath = '/'
+const publicPath = './'
 
 module.exports = {
   // 根据你的实际情况更改这里
   publicPath,
   lintOnSave: true,
   productionSourceMap: false,
-  devServer: {
-    publicPath // 和 publicPath 保持一致
+  configureWebpack() {
+    return {
+      resolve: {
+        alias: {
+          '@': resolve('src')
+        }
+      }
+    }
   },
   css: {
+    requireModuleExtension: true,
     loaderOptions: {
       // 设置 scss 公用变量文件
-      sass: {
-        prependData: `@import '@/theme/variables.scss';`
+      scss: {
+        /*sass-loader 8.0语法 */
+        // prependData: `@import '~@/theme/variables.scss';`
+
+        /*sass-loader 9.0写法*/
+        additionalData(content, loaderContext) {
+          const { resourcePath, rootContext } = loaderContext
+          const relativePath = path.relative(rootContext, resourcePath)
+          if (relativePath.replace(/\\/g, '/') !== 'src/theme/variables.scss') {
+            return '@import "~@/theme/variables.scss";' + content
+          }
+          return content
+        }
       }
     }
   }
