de19ef9d2a49753049d10c16784e6a3b81e57ead
Jonathan143 <1439821144@qq.com>
2021-07-08 09:28:57 +0800

    chore: update package

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import styleImport from 'vite-plugin-style-import'
 
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     styleImport({
       libs: [
         {
