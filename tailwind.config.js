d266f602342c4531414dbe86f92f05963c9f70fe
Jonathan143 <1439821144@qq.com>
2021-12-26 22:04:11 +0800

    chore: update tailwindcss & add echarts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     fontSize: {
       xs: '12px',
@@ -65,9 +68,6 @@ module.exports = {
         10: '40px',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
